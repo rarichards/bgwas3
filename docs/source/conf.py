f827e4d72ed68a2b10557379745b11bd37373e6a
Gregory Leeman <gregory@dyn3166-174.wlan.ic.ac.uk>
2019-07-01 13:55:54 +0100

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,6 +21,9 @@ project = 'bgwas3'
 copyright = '2019, Gregory Leeman'
 author = 'Gregory Leeman'
 
+# The full version, including alpha/beta/rc tags
+release = '0.0.1'
+
 
 # -- General configuration ---------------------------------------------------
 
@@ -36,7 +39,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
