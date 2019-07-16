0808f65000a0bdff9efc0e637ac75ac02f6755f5
Gregory Leeman <gregoryleeman@outlook.com>
2019-07-16 20:30:06 +0100

    lazygit

--- bgwas3/bgwas3.py
+++ bgwas3/bgwas3.py
@@ -10,6 +10,9 @@ P.get_parameters(
   "../pipeline.yml",
   "pipeline.yml"])
 
+PY_SRC_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "python"))
+R_SRC_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "R"))
+
 # getContigs {{{
 @transform(
     input = "fastq",
@@ -21,27 +24,135 @@ def getContigs(infile, outfile):
     pass
 
 # }}}
-# getContigsList {{{
+# getKmers {{{
 @follows (
     getContigs
     )
 @transform(
     input = getContigs,
     filter = regex("contigs"),
-    output = r"list.txt"
+    output = r"kmers.gz"
     )
-def getContigsList(infile, outfile):
+def getKmers(infile, outfile):
+    ''' Kmer mining/ counting with fsm-lite '''
 
     statement = '''
-    ls %(infile) | awk -F. '{print $1 "\t" $0}' > %(outfile)
+    ls %(infile) | awk -F. '{print $1 "\t" $0}' > contig_list.txt &&
+    fsm-lite
+        -l contig_list.txt
+        -s 6
+        -S 610
+        -v
+        -t fsm_kmers
+    | gzip 
+        -c
+        > %(outfile)
     '''
 
     P.run(statement)
 
 # }}}
-# full {{{
+# getPhylogeny {{{
+@follows(getContigs)
+@transform(
+    input = getContigs,
+    filter = regex("contigs"),
+    output = r"phylogeny.tree"
+    )
+def getPhylogeny(infile, outfile):
+    
+    ''' Get phylogeny '''
+
+    pass
+
+# }}}
+# getDistances {{{
+@follows(getPhylogeny)
+@transform(
+    input = getPhylogeny,
+    filter = regex("phylogeny.tree"),
+    output = r"distances.tsv"
+    )
+def getDistances(infile, outfile):
+    
+    '''Get distances from a phylogeny tree that has been midpoint rooted'''
+
+    statement = '''
+    python %(PY_SRC_PATH)s/phylogeny_distance.py 
+        --calc-C %(infile)s 
+        > %(outfile)s
+    '''
+    
+    P.run(statement)
+
+# }}}
+# getPhenos {{{
+@split(
+    input = "pheno.tsv",
+    output = r"phenos/.*\.tsv"
+    )
+def getPhenos(infile, outfile):
+
+    statement = '''
+    Rscript %(R_SRC_PATH)s/split_tsv.R %(infile)s phenos
+    '''
+
+    P.run(statement)
+
+# }}}
+# getAssoc {{{
 @follows(
-    getContigsList
+    getKmers,
+    getDistances,
+    getPhenos
+    )
+@transform(
+    input = getPhenos,
+    filter = regex("phenos/(.*)\.tsv"),
+    output = r"\1\.assoc",
+    add_inputs = [getDistances, getKmers]
+    )
+def get_assoc(infiles, outfile):
+
+    phenos = infiles[0]
+    distances = infiles[1][0]
+    kmers = infiles[1][1]
+    
+    statement = '''
+    pyseer
+        --phenotypes=%(pheno)s
+        --kmers=%(kmers)s
+        --distances=%(distances)s
+        --min-af=0.01
+        --max-af=0.99
+        --cpu=15
+        --filter-pvalue=1E-8
+        > %(outfile)s
+    '''
+
+# }}}
+# getKmerAnnotation {{{
+@follows(
+    getAssoc,
+    )
+@transform(
+    input = getAssoc,
+    filter = regex("associations/(.*).assoc"),
+    output = r"annotated/\1\.txt",
+    add_inputs = ["ref_genomes"]
+    )
+def getKmerAnnotation(infiles, outfile):
+
+    ref_genomes = infiles[1][0]
+
+    statement = '''
+    ls %(ref_genomes) | awk '{print $1 "\t" ref}' > ref_genome_list.txt &&
+    python %(PY_SRC_PATH)/summarise_annotations.py
+    '''
+# }}}
+# full {{{
+@follows (
+    getKmerAnnotation
     )
 def full():
     pass
--- bgwas3/entry.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-def main():
-    bgwas3.main(sys.argv)
-
-if __name__ == "__main__":
-    sys.exit(main())
