47bf4e400e290b9f9bbe33919bba310daba449f3
Gregory Leeman <g-r-eg@outlook.com>
2019-08-29 16:12:11 +0100

    lazygit

--- bgwas3/R/bonferoni.R
+++ bgwas3/R/bonferoni.R
@@ -4,24 +4,31 @@ suppressWarnings(suppressMessages(library(argparse)));
 suppressWarnings(suppressMessages(library(dplyr)));
 suppressWarnings(suppressMessages(library(readr)));
 
-
-parser <- ArgumentParser();
-
-parser$add_argument('file_patterns', help='patterns file');
-parser$add_argument('out_file', help='out file (stats)');
-parser$add_argument('--alpha', default=0.05, help='family-wise error rate');
-
-args <- parser$parse_args()
-
-statement <- paste0("LC_ALL=C sort -u -S 2014M ", args$file_patterns, " | wc -l");
-unique_patterns <- strtoi(system(statement, intern=TRUE));
-bonf_thresh <- 0;
-if(unique_patterns != 0){
-	bonf_thresh <- args$alpha/unique_patterns;
+get_args <- function(){
+	parser <- ArgumentParser();
+	parser$add_argument('patterns', help='patterns file path', default="patterns.txt");
+	parser$add_argument('--output', help='out file path', default="bonf.txt");
+	parser$add_argument('--alpha', default=0.05, help='family-wise error rate');
+	return(parser$parse_args());
 }
 
-tibble(
-		stat=c("unique_patterns", "bonf_thresh"),
-		value=c(unique_patterns, bonf_thresh)
-		) %>% 
-	write_tsv(args$out_file);
+main <- function(path_patterns, path_output, alpha){
+	statement <- paste0("LC_ALL=C sort -u -S 2014M ", path_patterns, " | wc -l");
+	unique_patterns <- strtoi(system(statement, intern=TRUE));
+	bonf_thresh <- 0;
+	if(unique_patterns != 0){
+		bonf_thresh <- alpha/unique_patterns;
+	}
+	tibble(
+			stat=c("unique_patterns", "bonf_thresh"),
+			value=c(unique_patterns, bonf_thresh)
+	) %>% 
+		write_tsv(path_output);
+}
+
+args <- get_args();
+path_patterns <- args$patterns;
+path_output <- args$output;
+alpha <- args$alpha;
+
+main(args$patterns, args$output, args$alpha);
